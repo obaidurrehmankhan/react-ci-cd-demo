15064b138218a76cc85fcbae35afe9c5aeb462f9
UbaidUrRehmanKhan <ubaidur.rehman@yahoo.com>
2025-11-22 12:51:48 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
-// this is app component
+
 function App() {
 
   return (
     <>
-      <h1>CI/CD using Github Actions</h1>
-      <h2>For beginners v2</h2>
-      <h2>With 2 projects</h2>
+      <h1>Github Actions</h1>
+      <h2>For beginners</h2>
     </>
   )
 }
